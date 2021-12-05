33cd36caf829117ced40096cc3cd90c4bdcf78f8
IdanHarari11 <idanharari112@gmail.com>
2021-12-05 22:34:18 +0200

    n

---
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import Typical from "react-typical";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+import imgBack from "../../../src/images/mailz.jpeg";
+import mailTo from "../../../src/assets/AboutMe/mail.png";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import Footer from "../../PortfolioContainer/footer/Footer";
+import "./ContactMe.css";
+
+export default function ContactMe(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+  console.log(name);
+  const submitForm = async (e) => {
+    e.preventDefault();
+    try {
+      let data = {
+        name,
+        email,
+        message,
+      };
+      setBool(true);
+      const res = await axios.post(`/contact`, data);
+      if (name.length === 0 || email.length === 0 || message.length === 0) {
+        setBanner(res.data.msg);
+        toast.error(res.data.msg);
+        setBool(false);
+      } else if (res.status === 200) {
+        setBanner(res.data.msg);
+        toast.success(res.data.msg);
+        setBool(false);
+
+        setName("");
+        setEmail("");
+        setMessage("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="main-container fade-in" id={props.id || ""}>
+      <ScreenHeading subHeading={"Lets Keep In Touch"} title={"Contact Me"} />
+      <div className="central-form">
+        <div className="col">
+          <h2 className="title">
+            <Typical loop={Infinity} steps={["Get In Touch 📧", 1000]} />
+          </h2>{" "}
+          <a href="https://www.facebook.com/idan.harari.1" target="_blank">
+            <i className="fa fa-facebook-square" />
+          </a>
+          <a href="https://www.instagram.com/_idanharari/" target="_blank">
+            <i className="fa fa-instagram" />
+          </a>
+          <a href="mailto:idanharari112@gmail.com" target="_blank">
+            <i className="fa fa-linkedin" />
+          </a>
+          <a href="https://github.com/IdanHarari11" target="_blank">
+            <i className="fa fa-github" />
+          </a>
+        </div>
+        <div className="back-form">
+          <div className="img-back">
+            <h4>Send Your Email Here!</h4>
+            <img src={imgBack} alt="image not found" />
+          </div>
+          <form>
+            <div style={{ textAlign: "center" }}>
+              <h4>Click to send me an email !</h4> <br />
+              <a
+                style={{ cursor: "pointer" }}
+                href="mailto:idanharari112@gmail.com"
+              >
+                <img src={mailTo} style={{ width: "9rem", height: "9rem" }} />
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
